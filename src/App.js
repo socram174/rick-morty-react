81cdda20a9d69aa0b9735450830ad4ea67bdd573
socram174 <85124562+socram174@users.noreply.github.com>
2023-02-19 23:26:43 -0300

    Cambio pequeño con mui

--- src/App.js
+++ src/App.js
@@ -1,34 +1,52 @@
-import React from "react";
-import "./style.css";
-import { useEffect , useState} from 'react';
+import React from 'react';
+import './style.css';
+import { useEffect, useState } from 'react';
+import Button from '@mui/material/Button';
+import Chip from '@mui/material/Chip';
+import PersonIcon from '@mui/icons-material/Person';
 
 export default function App() {
   const [datos, setDatos] = useState([]);
-  const [colorB,setColor] = useState([]);
+  const [colorB, setColor] = useState([]);
   console.log(Math.floor(Math.random() * 10) + 1);
   const [num, setNum] = useState(292);
 
-  useEffect(()=> {
-    fetch("https://rickandmortyapi.com/api/character/"+num).then((res) => res.json()).then((data) => {console.log(data);
-  setDatos(data)
-  {data.status === "Alive" ? setColor("solid 5px green"): setColor("solid 5px red")}
-});
-  },[num]);
+  useEffect(() => {
+    fetch('https://rickandmortyapi.com/api/character/' + num)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setDatos(data);
+        {
+          data.status === 'Alive'
+            ? setColor('solid 5px green')
+            : setColor('solid 5px red');
+        }
+      });
+  }, [num]);
   return (
-    <div style={{border:colorB}}>
+    <div style={{ border: colorB }}>
       <h1>Rick & Morty API</h1>
-      <h2>{datos.name} / {datos.status === "Alive" ? (
-        <span style={{color: "green"}}>{datos.status}</span>
-      ) : (
-        <span style={{color: "red"}}>{datos.status}</span>
-      )}</h2>
+      <h2>
+        {datos.name} /{' '}
+        {datos.status === 'Alive' ? (
+          <Chip label={datos.status} color="success" size="small" />
+        ) : (
+          <Chip label={datos.status} color="error" size="small" />
+        )}
+      </h2>
 
-      <img src={datos.image}/>
-      <h1>____________</h1>
+      <img src={datos.image} />
 
-      <button onClick={()=> {
-        setNum(Math.floor(Math.random() * 500)+ 1);
-      }}>Random Character</button>
+      <Button
+        onClick={() => {
+          setNum(Math.floor(Math.random() * 500) + 1);
+        }}
+        variant="contained"
+        color="success"
+      >
+        <PersonIcon /> Random Character
+      </Button>
     </div>
   );
 }
