6099b1e20f922c940d0ec1133e01fb7a1ee56658
socram174 <85124562+socram174@users.noreply.github.com>
2023-02-19 23:48:45 -0300

    mejoras de la interfaz

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ import Button from '@mui/material/Button';
 import Chip from '@mui/material/Chip';
 import PersonIcon from '@mui/icons-material/Person';
 
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+
 export default function App() {
   const [datos, setDatos] = useState([]);
   const [colorB, setColor] = useState([]);
@@ -47,6 +53,29 @@ export default function App() {
       >
         <PersonIcon /> Random Character
       </Button>
+
+      <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          image={datos.image}
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {datos.name} / {datos.status === 'Alive' ? (
+          <Chip label={datos.status} color="success" size="small" />
+        ) : (
+          <Chip label={datos.status} color="error" size="small" />
+        )}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
     </div>
   );
 }
